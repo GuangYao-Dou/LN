9eb0856cf496c450ad57d92c0a7512257d50e2ff
Vincent-Dou <44737368+Vincent-Dou@users.noreply.github.com>
2019-04-10 09:06:30 +0800

    niuke

---
+++ NiuKe8.java
@@ -0,0 +1,24 @@
+package niuke;
+import java.util.Stack;
+/**
+ * Auther:vincent-Dou
+ * Date: 2019/4/10
+ * Time: 9:04
+ * description：用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
+ */
+public class NiuKe8 {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        if(stack2.empty()){
+            while(!stack1.empty())
+                stack2.push(stack1.pop());
+        }
+        return stack2.pop();
+    }
+}
