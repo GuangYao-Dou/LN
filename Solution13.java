d508cbad113b8bf72a4c968830b15cadbc448ff0
Vincent-Dou <44737368+Vincent-Dou@users.noreply.github.com>
2019-04-14 14:22:02 +0800

    0414

---
+++ Solution13.java
@@ -0,0 +1,69 @@
+package LeetCode;
+
+/**
+ * Auther:vincent-Dou
+ * Date: 2019/4/14
+ * Time: 14:19
+ * description：罗马数字转整数
+ */
+public class Solution13 {
+    public int romanToInt(String s) {
+        // small(s);
+        char str[] = s.toCharArray();
+        int sum = 0;
+        for(int i = 0; i < str.length; i++){
+            String tem = "";
+            if(i+1<str.length){
+                tem = str[i]+""+str[i+1];
+            }
+            if(small(tem) != 0) {
+                sum = sum + small(tem);
+                i++;
+            }else {
+                if(str[i] == 'I'){
+                    sum = sum + 1;
+                }
+                if(str[i] == 'V'){
+                    sum = sum + 5;
+                }
+                if(str[i] == 'X'){
+                    sum = sum + 10;
+                }
+                if(str[i] == 'L'){
+                    sum = sum + 50;
+                }
+                if(str[i] == 'C'){
+                    sum = sum + 100;
+                }
+                if(str[i] == 'D'){
+                    sum = sum + 500;
+                }
+                if(str[i] == 'M'){
+                    sum = sum + 1000;
+                }
+            }
+        }
+        return sum;
+    }
+    public static int small(String s){
+        if(s.equals("IV")){
+            return 4;
+        }
+        if(s.equals("IX")){
+            return 9;
+        }
+        if(s.equals("XL")){
+            return 40;
+        }
+        if(s.equals("XC")){
+            return 90;
+        }
+        if(s.equals("CD")){
+            return 400;
+        }
+        if(s.equals("CM")){
+            return 900;
+        }
+        return 0;
+    }
+}
